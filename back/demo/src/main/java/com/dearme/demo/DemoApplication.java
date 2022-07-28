e2e28d064f76d4205e77e2fdc836fb27c56d9488
goodman1223 <goodman1223@naver.com>
2022-07-28 09:48:56 +0900

    Squashed commit of the following:
    commit bcd826c5eb768c36051206c050e792e4aeb3b17a
    Author: goodman1223 <goodman1223@naver.com>
    Date:   Thu Jul 28 09:47:22 2022 +0900
    
        [feat] : JPA Auditing 기능 추가
    
        CreatedDate, LastModifiedDate를 Base Entity로 둠
    
    commit e052d18da4cb74f4c0558bc0b80f39585ba3d7a7
    Author: goodman1223 <goodman1223@naver.com>
    Date:   Thu Jul 28 09:42:51 2022 +0900
    
        [feat] : Swagger Config 설정
    
    commit 0850d538564c9640665b775adf32406e660ffb5f
    Author: goodman1223 <goodman1223@naver.com>
    Date:   Thu Jul 28 09:42:29 2022 +0900
    
        [build] swagger 의존성 추가

--- back/demo/src/main/java/com/dearme/demo/DemoApplication.java
+++ back/demo/src/main/java/com/dearme/demo/DemoApplication.java
@@ -2,8 +2,12 @@ package com.dearme.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
+@EnableJpaAuditing
 public class DemoApplication {
 
     public static void main(String[] args) {
