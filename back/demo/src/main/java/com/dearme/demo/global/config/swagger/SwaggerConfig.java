e052d18da4cb74f4c0558bc0b80f39585ba3d7a7
goodman1223 <goodman1223@naver.com>
2022-07-28 09:42:51 +0900

    [feat] : Swagger Config 설정

---
+++ back/demo/src/main/java/com/dearme/demo/global/config/swagger/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.dearme.demo.global.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    Docket api(){
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .build();
+    }
+}
