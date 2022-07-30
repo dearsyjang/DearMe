d2572758044eda5467661dc8b943814062be2d13
ChoiJiSang <cltkddl21@naver.com>
2022-07-31 04:22:42 +0900

    S07P12D206-144 [feat] : 로그인 페이지 추가
    로그인 페이지에서 로그인 할 수 있음.

--- frontend/src/views/member/LoginView.vue
+++ frontend/src/views/member/LoginView.vue
@@ -1,22 +1,45 @@
 <template>
   <div>
     <h1> Dear Me </h1>
-    <h2>아이디</h2>
-      
+      <form @submit.prevent="login(credentials)">
+        <div>
+        <label for="username">아이디 </label> <br>
+        <input v-model="credentials.username" type="text" id="username" required />
+      </div>
+
+      <div>
+        <label for="password">비밀번호 </label> <br>
+        <input v-model="credentials.password" type="password" id="password" required />
+      </div>
+      <br>
+
+      <button>로그인</button> 
+      </form>
+      <br>
+      <button>아이디 찾기</button> &emsp;
+      <button>비밀번호 찾기</button>
   </div>
 </template>
 <script>
+import { mapActions } from 'vuex'
+
 export default {
+  name: 'LoginView',
   components: {},
   data() {
     return {
-      sampleData: ''
+      credentials: {
+        username: '',
+        password: '',
+      }
     }
   },
   setup() {},
   created() {},
   mounted() {},
   unmounted() {},
-  methods: {}
+  methods: {
+    ...mapActions(['login'])
+  },
 }
 </script>
