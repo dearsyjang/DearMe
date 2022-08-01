286ebad7d2d2747fce2c3fb11d99eb4688c7622e
이기종 <rl00822@naver.com>
2022-08-01 21:15:16 +0900

    S07P12D206-253 [feat] : 게시판 작성 기능 추가

--- back/demo/src/main/java/com/dearme/demo/domain/board/controller/BoardApiController.java
+++ back/demo/src/main/java/com/dearme/demo/domain/board/controller/BoardApiController.java
@@ -1,5 +1,6 @@
 package com.dearme.demo.domain.board.controller;
 
+import com.dearme.demo.domain.board.dto.BoardSaveRequestDto;
 import com.dearme.demo.domain.board.service.BoardService;
 import com.dearme.demo.global.common.CommonResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -7,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -15,6 +17,11 @@ import org.springframework.web.bind.annotation.*;
 public class BoardApiController {
     private final BoardService boardService;
 
+    @Operation(summary = "Board Save Test", description = "게시판 글 저장")
+    @PostMapping
+    public ResponseEntity<CommonResponse> boardSave(@RequestBody @Validated BoardSaveRequestDto dto){
+        return new ResponseEntity<>(CommonResponse.getSuccessResponse(boardService.boardSave(dto)), HttpStatus.OK);
+    }
     @Operation(summary = "Board View Test", description = "게시판 전체 조회")
     @GetMapping
     public ResponseEntity<CommonResponse> boardView(@RequestParam("page") Integer page, @RequestParam("size") Integer size){
