a26b77526ac1dfd2f9a5469d29c2d21e3108ffa9
이상민 <goodman1223@naver.com>
2022-08-02 14:21:42 +0900

    S07P12D206-120/feature/user/login 로그인 요청 API 개발

--- back/demo/src/main/java/com/dearme/demo/domain/user/service/UserService.java
+++ back/demo/src/main/java/com/dearme/demo/domain/user/service/UserService.java
@@ -1,8 +1,11 @@
 package com.dearme.demo.domain.user.service;
 
+import com.dearme.demo.domain.user.dto.LoginRequestDto;
+import com.dearme.demo.domain.user.dto.LoginResponseDto;
 import com.dearme.demo.domain.user.dto.SignUpRequestDto;
 import com.dearme.demo.domain.user.dto.SignUpResponseDto;
 
 public interface UserService {
     SignUpResponseDto signUpUser(SignUpRequestDto dto);
+    LoginResponseDto login(LoginRequestDto dto);
 }
