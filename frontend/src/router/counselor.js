98aab926a70022ad44d06f06a6970456923bb7c0
rainbow7417 <rainbow7417@naver.com>
2022-08-03 14:17:09 +0900

    front-develop 갱신

--- frontend/src/router/counselor.js
+++ frontend/src/router/counselor.js
@@ -1,6 +1,6 @@
 import CounselorListView from '@/views/counselor/CounselorListView.vue'
 import CounselorDetailView from '@/views/counselor/CounselorDetailView.vue'
-
+import FileFormComp from '@/views/counselor/components/FileFormComp'
 export default [
     {
         path: '/counselor_list',
@@ -12,5 +12,10 @@ export default [
         name: 'counselor',
         component: CounselorDetailView
       },
+      {
+        path: '/counselors/attachFile',
+        name: 'attachfile',
+        component: FileFormComp
+      }
     
   ]
