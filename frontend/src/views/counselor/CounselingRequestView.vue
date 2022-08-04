481aee25e129c80977284b4a3dc8da71835c2150
ChoiJiSang <cltkddl21@naver.com>
2022-08-04 21:13:35 +0900

    S07P12D206-188 [feat] : 신청 기능 추가
    신청버튼 눌러서 내용 넘김.

--- frontend/src/views/counselor/CounselingRequestView.vue
+++ frontend/src/views/counselor/CounselingRequestView.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
     <h1>상담 신청하기</h1>
-        <br>
-        <p>신청자 : 이름 떠야됨</p>
-        <br>
-        <span></span>
-        <p style="white-space: pre-line">{{ message }}</p>
-        <br>
-        <textarea v-model="message" placeholder="내용을 작성하시오..."></textarea>
-        <br>
-        <div class="form-check form-switch">
+        <p>신청자 : {{ userName }}</p>
+        <textarea style="width:40%" v-model="message" placeholder="내용을 작성하시오..."></textarea>
+        <div class="form-switch">
             <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
-            <label class="form-check-label" for="flexSwitchCheckChecked"></label>
+            <label class="form-check-label" for="flexSwitchCheckChecked">개인정보 공개</label>
         </div>
   </div>
+  <div>
+    <!-- 달력자리~ -->
+
+    <!-- 시간자리~ -->
+  </div>
+  <button @click="test">신청</button>
 </template>
 
 <script>
@@ -21,14 +21,19 @@
   components: {},
   data() {
     return {
-      sampleData: ''
+      message: '이거는 넘겨줘야해',
+      userName: '이거는 위에서 땡겨와야대'
     }
   },
   setup() {},
   created() {},
   mounted() {},
   unmounted() {},
-  methods: {}
+  methods: {
+    test () {
+      console.log('확인용', this.message)
+    }
+  }
 }
 </script>
 
