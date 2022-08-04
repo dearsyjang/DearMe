60d3e1481c0c6551a30c0fe3fcd8ec6003d360c1
ChoiJiSang <cltkddl21@naver.com>
2022-08-04 23:55:57 +0900

    first commit

---
+++ frontend/src/views/counseling-request/CounselingRequestListView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      sampleData: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
