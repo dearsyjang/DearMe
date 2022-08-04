0ccebd152ba1d6401029fbdf41b86e6de10afb86
ChoiJiSang <cltkddl21@naver.com>
2022-08-04 09:49:08 +0900

    S07P12D206-147 [feat] : 비밀번호 찾기 기능 추가
    아이디와 이메일을 입력하여 비밀번호를 확인

---
+++ frontend/src/views/member/FindpwView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>비밀번호 찾기</h1>
+  </div>
+   <br>
+  <div>
+    <label for="username">아이디 </label> <br>
+    <input  v-model="credentials.username" type="text" id="username" required/>
+  </div>
+   <br>
+  <div class="input-group mb-3">
+    <label for="e-mail">이메일</label> <br>
+    <input type="text" class="form-control">
+     <span class="input-group-text">@</span>
+    <input type="text" class="form-control">
+  </div>
+
+  <div>
+    <button>비밀번호 찾기</button>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      sampleData: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
