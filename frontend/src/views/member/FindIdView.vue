1f1d6c3147d5977a9a8e48149e06b41284c5a311
ChoiJiSang <cltkddl21@naver.com>
2022-08-04 09:17:46 +0900

    S07P12D206-145 [feat] : 이메일 입력 기능 추가
    이메일을 입력해서 아이디찾기 버튼을 누름.

---
+++ frontend/src/views/member/FindIdView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>아이디 찾기</h1>
+
+    <div>
+        <div class="input-group mb-3">
+        <label for="e-mail">이메일</label> <br>
+        <input type="text" class="form-control">
+        <span class="input-group-text">@</span>
+        <input type="text" class="form-control">
+        </div>
+    </div>
+    <br>
+    <div>
+        <button>아이디 찾기</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      sampleData: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
