8afa7f69f0b85f9fb5101b80179a57da6164ec10
rl00822 <rl00822@naver.com>
2022-08-04 15:04:25 +0900

    S07P12D206-139 [feat] : STT 요청 기능 추가

--- back/demo/src/main/java/com/dearme/demo/domain/board/exception/board/NoBoardSavePermissionException.java
+++
@@ -1,7 +0,0 @@
-package com.dearme.demo.domain.board.exception.board;
-
-public class NoBoardSavePermissionException extends RuntimeException{
-    public NoBoardSavePermissionException() {
-        super("상담사는 게시글을 작성할 수 없습니다.");
-    }
-}
--- back/demo/src/main/java/com/dearme/demo/domain/board/exception/comment/NoCommentSavePermissionException.java
+++
@@ -1,7 +0,0 @@
-package com.dearme.demo.domain.board.exception.comment;
-
-public class NoCommentSavePermissionException extends RuntimeException{
-    public NoCommentSavePermissionException() {
-        super("상담사 이외에는 댓글을 작성할 수 없습니다.");
-    }
-}
