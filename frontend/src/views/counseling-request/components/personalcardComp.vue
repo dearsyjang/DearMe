1368dc51ed312e3f41e0763ad6f44557574f3b87
ChoiJiSang <cltkddl21@naver.com>
2022-08-05 13:12:48 +0900

    S07P12D206-292 [feat] : 상담 대기목록  페이지 작성

--- frontend/src/views/counseling-request/components/personalcardComp.vue
+++ frontend/src/views/counseling-request/components/personalcardComp.vue
@@ -1,9 +1,16 @@
 <template>
     <div>
         <div class="card w-50">
-            <div class="card-body">
-                <h5 class="card-title">해당 닉네임</h5>
-                <p class="card-text">해당 상담일</p>
+            <div class="card-body1">
+                <h5 class="card-title">해당 닉네임1</h5>
+                <p class="card-text">해당 상담일1</p>
+                <a href="#" class="btn btn-primary">신청서 보기</a>
+            </div>
+        </div>
+        <div class="card w-50">
+            <div class="card-body2">
+                <h5 class="card-title">해당 닉네임2</h5>
+                <p class="card-text">해당 상담일2</p>
                 <a href="#" class="btn btn-primary">신청서 보기</a>
             </div>
         </div>
