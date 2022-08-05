72958e2e51e6d91dcde516d4a0a811f9802c7228
YOONHAE <ue0828@naver.com>
2022-08-05 20:14:05 +0900

    S07P12D206-152 [feat] : api 연결 실패

--- frontend/src/api/drf.js
+++ frontend/src/api/drf.js
@@ -1,14 +1,12 @@
-const HOST = 'http://localhost:8080/api/'
-
-const MEMBER = 'member/'
+// const HOST = 'http://localhost:8080/api/'
+const HOST = "https://i7d206.p.ssafy.io/"
+const MEMBER = 'users/'
 
 export default {
   member : {
-    login: () => HOST + MEMBER + 'login/',
-    logout: () => HOST + MEMBER + 'logout/',
-    signup: () => HOST + MEMBER + 'signup/',
-    currentUserInfo: () => HOST + MEMBER + 'user/',
-    profile: username => HOST + MEMBER + 'profile/' + username,
-    isAdmin: username => HOST + MEMBER + 'isadmin/' + username,
+    login: () => HOST + MEMBER + 'token/',
+    logout: () => HOST + MEMBER + 'token/',
+    signup: () => HOST + MEMBER,
+    currentUserInfo: () => HOST + MEMBER + 'token/',
   },
 }
---
+++ frontend/src/views/board/components/ArticleForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="article-form">
+    <br>
+    <h1>게시글 작성하기</h1>
+    <!--v-if="action"==""으로 작성, 수정 구분해주기-->
+    <hr>
+    <div class="container">
+        <h4 id="article-form-title-text">제목</h4> 
+        <form class="article-form">
+            <input id="article-form-title" type="text" v-model="title" placeholder="제목을 입력해주세요.">
+            <br>
+            <br>
+            <h4 id="article-form-content-text">내용</h4>
+            <textarea id="article-form-content" rows="10" max-rows="10" v-model="contents" placeholder="내용을 입력해주세요."></textarea>
+            <br>
+            <div>
+                <button type="button" class="btn" id="article-form-submit-button" v-on:click="fnSave">저장</button>
+                <router-link :to="{ name: 'board' }"><button button type="button" class="btn" id="article-form-cancel-button">취소</button></router-link>
+            </div>
+        </form> 
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#article-form-title, #article-form-content {
+    width: 100%;
+}
+
+#article-form-title-text, #article-form-content-text{
+  text-align: start;
+}
+
+#article-form-submit-button {
+  width:auto; height:50px;
+  background-color: #3282B8;
+  margin: 3px;
+  outline: none;
+}
+
+#article-form-cancel-button{
+  width:auto; height:50px;
+  background-color: #B83232;
+  margin: 3px;
+  outline: none;
+}
+
+</style>
