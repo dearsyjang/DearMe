621f11284eb0e6b3db1a3b600536cc98f44d33fa
YOONHAE <ue0828@naver.com>
2022-08-05 09:49:56 +0900

    S07P12D206-152 [feat] : 회원가입, 로그인, 아이디 비번찾기 페이지 생성, 회원가입 데이터 연결 아직

--- frontend/src/views/mypage/user/components/infoComp.vue
+++ frontend/src/views/mypage/user/components/infoComp.vue
@@ -1,17 +1,13 @@
 <template>
-  <div class="Info">
-    <div class="d-flex justify-content-around">
-      <div class="mx-4" id="label">
-        <p for="nickname">닉네임</p>
-        <p for="point">잔여 포인트</p>
-      </div>
-      <div id="content">
-        <P>사용자</P>
-        <p>500 pt</p>
-      </div>
-      <div class="mx-5" id="btn">
-        <button class="btn btn-primary btn-sm align-self-end">충전하기</button>
-      </div>
+  <div class="Info container">
+    <div class="form-group">
+      <label class="form-label" for="nickname">닉네임</label>
+      <p class="form-control">사용자</p>
+    </div>
+    <div class="form-group">
+      <label class="form-label" for="myPoint">잔여포인트</label>
+      <p class="form-control">500 pt</p>
+      <button class="btn btn-primary">충전하기</button>
     </div>
   </div>
 </template>
