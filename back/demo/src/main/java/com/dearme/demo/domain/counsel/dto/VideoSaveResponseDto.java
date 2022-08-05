9382e0418313f03a0f6ee5f51246a4794dbf56d4
이기종 <rl00822@naver.com>
2022-08-05 14:16:28 +0900

    S07P12D206-241 [feat] : STT 요청 및 감정 분석 기능 추가, 리뷰 기능 추가 및 의존성 추가 예외 처리 추가

---
+++ back/demo/src/main/java/com/dearme/demo/domain/counsel/dto/VideoSaveResponseDto.java
@@ -0,0 +1,11 @@
+package com.dearme.demo.domain.counsel.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class VideoSaveResponseDto {
+    private String videotext;
+}
