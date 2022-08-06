9906aa8da071c882cf0fa9d46af98575a887e5e4
Sangmeeeee <goodman1223@naver.com>
2022-08-06 15:14:52 +0900

    S07P12D206-150 [feat] : TextDiaryList 로직 구현

--- back/demo/src/main/java/com/dearme/demo/domain/textdiary/service/TextDiaryService.java
+++ back/demo/src/main/java/com/dearme/demo/domain/textdiary/service/TextDiaryService.java
@@ -3,9 +3,12 @@ package com.dearme.demo.domain.textdiary.service;
 import com.dearme.demo.domain.textdiary.dto.PostTextDiaryRequestDto;
 import com.dearme.demo.domain.textdiary.dto.PostTextDiaryResponseDto;
 import com.dearme.demo.domain.textdiary.dto.TextDiaryDetailsResponseDto;
+import com.dearme.demo.domain.textdiary.dto.TextDiaryListResponseDto;
 
 public interface TextDiaryService {
     PostTextDiaryResponseDto postTextDiary(String id, PostTextDiaryRequestDto dto);
 
     TextDiaryDetailsResponseDto getDetails(String id, Long textDiaryId);
+
+    TextDiaryListResponseDto getList(String id, Integer month);
 }
