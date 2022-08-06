e563f136964d651e08ac15495d6c0f18c7bd5f80
Sangmeeeee <goodman1223@naver.com>
2022-08-07 03:21:22 +0900

    S07P12D206-429 [feat] : 그룹 생성 요청 로직 구현

---
+++ back/demo/src/main/java/com/dearme/demo/domain/group/service/GroupServiceImpl.java
@@ -0,0 +1,34 @@
+package com.dearme.demo.domain.group.service;
+
+import com.dearme.demo.domain.group.dto.CreateGroupRequestDto;
+import com.dearme.demo.domain.group.dto.CreateGroupResponseDto;
+import com.dearme.demo.domain.group.entity.Group;
+import com.dearme.demo.domain.group.exception.UserCreateGroupException;
+import com.dearme.demo.domain.group.repository.GroupRepository;
+import com.dearme.demo.domain.user.entity.Type;
+import com.dearme.demo.domain.user.entity.User;
+import com.dearme.demo.domain.user.exception.NoExistUserException;
+import com.dearme.demo.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GroupServiceImpl implements GroupService{
+
+    private final UserRepository userRepository;
+    private final GroupRepository groupRepository;
+
+    @Override
+    public CreateGroupResponseDto createGroup(String id, CreateGroupRequestDto dto) {
+        User counselor = userRepository.findUserById(id).orElseThrow(() -> {
+            throw new NoExistUserException();
+        });
+        if(counselor.getType().equals(Type.USER)){
+            throw new UserCreateGroupException();
+        }
+        Group group = dto.toEntity();
+        group.setCounselor(counselor);
+        return new CreateGroupResponseDto(groupRepository.save(group).getId());
+    }
+}
