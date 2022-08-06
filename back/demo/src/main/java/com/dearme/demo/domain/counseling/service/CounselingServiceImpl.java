e010ea80903bda0c155f903e2e7316018c1b86f5
Sangmeeeee <goodman1223@naver.com>
2022-08-06 22:15:08 +0900

    S07P12D206-231 [feat] : Counseling List 받아오는 기능 추가

--- back/demo/src/main/java/com/dearme/demo/domain/counseling/service/CounselingServiceImpl.java
+++ back/demo/src/main/java/com/dearme/demo/domain/counseling/service/CounselingServiceImpl.java
@@ -1,10 +1,15 @@
 package com.dearme.demo.domain.counseling.service;
 
+import com.dearme.demo.domain.counseling.dto.CounselingInfoListResponseDto;
+import com.dearme.demo.domain.counseling.dto.CounselingInfoResponseDto;
 import com.dearme.demo.domain.counseling.entity.Counseling;
 import com.dearme.demo.domain.counseling.repository.CounselingRepository;
 import com.dearme.demo.domain.counselingdocument.entity.CounselingDocument;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +19,16 @@ public class CounselingServiceImpl implements CounselingService{
     @Override
     public void createCounseling(CounselingDocument counselingDocument) {
         Counseling counseling = counselingDocument.toCounselingEntity();
+        counseling.setCounselingDocument(counselingDocument);
         counselingRepository.save(counseling);
+    }
+
+    public CounselingInfoListResponseDto getCounselings(String id) {
+        List<Counseling> counselings = counselingRepository.findAllByCounselor_Id(id);
+        List<CounselingInfoResponseDto> counselingInfoResponseDtos = new ArrayList<>();
+        for(Counseling counseling : counselings){
+            counselingInfoResponseDtos.add(CounselingInfoResponseDto.of(counseling));
+        }
+        return new CounselingInfoListResponseDto(counselingInfoResponseDtos);
     }
 }
