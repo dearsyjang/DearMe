4b30a7dd82e6d4a5afdabb75b6463818f768aa38
goodman1223 <goodman1223@naver.com>
2022-08-08 16:21:07 +0900

    S07P12D206-449 [feat] : Career Update 로직 추가

--- back/demo/src/main/java/com/dearme/demo/domain/user/repository/CareerRepository.java
+++ back/demo/src/main/java/com/dearme/demo/domain/user/repository/CareerRepository.java
@@ -3,5 +3,8 @@ package com.dearme.demo.domain.user.repository;
 import com.dearme.demo.domain.user.entity.Career;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CareerRepository extends JpaRepository<Career, Long> {
+    Optional<Career> findCareerByCounselorProfile_Counselor_IdAndId(String counselorId, Long id);
 }
