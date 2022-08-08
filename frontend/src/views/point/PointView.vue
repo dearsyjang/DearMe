ef5782434911ecc3b5769934f0e60aad1524dab6
rainbow7417 <rainbow7417@naver.com>
2022-08-08 23:43:10 +0900

    카카오페이 포인트 충전

--- frontend/src/views/point/PointView.vue
+++ frontend/src/views/point/PointView.vue
@@ -3,9 +3,9 @@
   <section class="test">
     <select v-model="selected">
       <option disabled value="">충전할 포인트를 선택해 주세요.  </option>
-      <option>1000</option>
-      <option>2000</option>
-      <option>3000</option>
+      <option value = "1000">1000</option>
+      <option value = "2000">2000</option>
+      <option value = "3000">3000</option>
     </select>
     <br>
     <span>선택함 : {{ selected }}  point</span>
@@ -19,6 +19,7 @@
 
 
 <script>
+import { mapGetters, mapState } from 'vuex';
 export default {
   name: 'PointView',
   data(){
@@ -30,67 +31,84 @@ export default {
       }
     }
   },
-  methods: {
+  computed : {
+    ...mapState(['token']),
+    ...mapGetters(['authHeader']),
+  },
+  methods: { 
+    
     requestPay : function() {
+      console.log(this.token)
+      console.log(this.authHeader)
       /* 1. 가맹점 식별하기 */
-      const { IMP } = window;
+      const { IMP } = window;+
       IMP.init('TC0ONETIME');
+      /* 2. 데이터로 결제요청하기 */
+      IMP.request_pay({
+          pg : 'kakaopay',
+          pay_method : 'card',
+          merchant_uid: "order_no_0001", 
+          name : '주문명:결제테스트',
+          amount : this.selected,
+          buyer_email : this.userinfo.email,
+          buyer_name : this.userinfo.name,
+          m_redirect_url : 'http://localhost:8080/'
+          }, 
+          console.log(this.authHeader),
+          rsp => { // callback
+                const token = this.token
+                console.log(token)
+                const data = this.selected
+                console.log(data)
+                if (rsp.success) {
+                  fetch('http://localhost:8080/users/points', {         //json데이터 업로드.
+                  method: 'PUT',
+                  headers: {
+                    'Content-Type': 'applications/json',
+                    Authorization : token
+                  },
+                  body: JSON.stringify(data),
+                  })
+                  .then((response)=> response.json())
+                  .then((data)=> {
+                    console.log('hi', data)
+                    alert("완료 -> imp_uid : " + rsp.imp_uid+" / merchant_uid(orderKey) : " + rsp.merchant_uid);
+                  })
+                  .catch((error) => {
+                    console.log('bye', error)
+                    alert("실패 : 코드(" + rsp.error_code + ") / 메시지(" + rsp.error_msg + ")");
+                  })
+                }
 
-      /* 2. 결제 데이터 정의하기 */
-       IMP.request_pay({
-        pg:'kakaopay',                       
-        pay_method: 'card',                     
-        merchant_uid: `mid_${new Date().getTime()}`, 
-        amount: this.selected,                                
-        name: '아임포트 결제',               
-        buyer_name: this.userinfo.name,                      
-        buyer_email: this.userinfo.email,               
-                   
+            })
+          }
+        }
+  }
+</script>
+      
+    //     function(rsp) { // callback
+    //         console.log(this.selected)
+    //         let amount = this.selected
+    //         console.log(amount)
+    //         if (rsp.success) {
+    //         fetch('http://localhost:8080/users/points', {
+    //             method: 'PUT',
+    //             headers: this.authHeader,
+    //             body: JSON.stringify({price: amount})
+    //         }).then((rsp) => rsp.json()).then((amount) => {
+    //             console.log(amount);
+    //         });
+    //         alert("완료 -> imp_uid : " + rsp.imp_uid+" / merchant_uid(orderKey) : " + rsp.merchant_uid);
         
-      }, 
-        function(rsp) { // callback
-            let amount=this.amount
-            console.log(amount)
-            if (rsp.success) {
-            fetch('http://localhost:8080/users/points', {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization' : 'ad eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImlkIiwiaXNzIjoiZGVhcm1lIiwiaWF0IjoxNjU5NjgwMzg0LCJleHAiOjE2NTk2ODM5ODR9.izg6xcOPKIdxiU6QjTQ2GoVKFqSsBwBgyP4FCd6uibQ'
-                },
-                body: JSON.stringify({price: amount})
-            }).then((rsp) => rsp.json()).then((amount) => {
-                console.log(amount);
-            });
-            alert("완료 -> imp_uid : " + rsp.imp_uid+" / merchant_uid(orderKey) : " + rsp.merchant_uid);
-        
-      } else {
-        alert("실패 : 코드(" + rsp.error_code + ") / 메시지(" + rsp.error_msg + ")");
-        alert(`결제 실패`);
-      }
-    })
+    //   } else {
+    //     alert("실패 : 코드(" + rsp.error_code + ") / 메시지(" + rsp.error_msg + ")");
+    //     alert(`결제 실패`);
+    //   }
+    // })
+    
     },
   }
 }
-    //   rsp => {
-    //     const selected = this.selected
-    //     if(rsp.success){
-            // fetch('http://localhost:8080/users/points', {
-            //     method: 'PUT',
-            //     headers: {
-            //         'Content-Type': 'application/json',
-            //         'Authorization' : 'ad eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImlkIiwiaXNzIjoiZGVhcm1lIiwiaWF0IjoxNjU5NjgwMzg0LCJleHAiOjE2NTk2ODM5ODR9.izg6xcOPKIdxiU6QjTQ2GoVKFqSsBwBgyP4FCd6uibQ'
-            //     },
-            //     body: JSON.stringify({price: selected})
-            // }).then((response) => response.json()).then((selected) => {
-            //     console.log(selected);
-            // });
-            // alert("완료 -> imp_uid : " + rsp.imp_uid+" / merchant_uid(orderKey) : " + rsp.merchant_uid);
-    //     }else{
-            // alert("실패 : 코드(" + rsp.error_code + ") / 메시지(" + rsp.error_msg + ")");
-    //     }
-    // }
-//   },
-// },
-
-</script>
+    
+<!-- 
+</script> -->
