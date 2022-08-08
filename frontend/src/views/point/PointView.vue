0f27bc5a89525a4879c4e4e92f505842e1c40988
강지명 <rainbow7417@naver.com>
2022-08-08 16:25:47 +0900

    Resolve S07P12D206-143 "/feature/point/pointcharge"

--- frontend/src/views/point/PointView.vue
+++ frontend/src/views/point/PointView.vue
@@ -1,67 +1,96 @@
 <template>
 
-<div class = "point">  
-  <div class="card">
-      <div class="card-body">
-      <h3>포인트 충전</h3>    
-      </div>
-  </div>
+  <section class="test">
+    <select v-model="selected">
+      <option disabled value="">충전할 포인트를 선택해 주세요.  </option>
+      <option>1000</option>
+      <option>2000</option>
+      <option>3000</option>
+    </select>
+    <br>
+    <span>선택함 : {{ selected }}  point</span>
+      
+    <button @click="requestPay">
+      결제하기
+    </button>
+  </section>
 
-  <div class="card">
-    <div class="card-body">
-      <p>
-        ※ 충전된 포인트는 상담 신청시 차감됩니다.
-      </p> 
-      <p>
-        ※ 결제가 완료된 포인트는 환불되지 않으니 유의하시기 바랍니다. 
-      </p>
-      <p>
-        ※ 1포인트 = 1000원
-      </p>    
-      <p>
-        ※ 100포인트(10만원)이상 결제시 10% 할인
-      </p>
-      <p>
-        500포인트(50만원)이상 결제시 15% 할인
-      </p>
-      <p>
-        1000포인트(100만원)이상 결제시 20% 할인 
-      </p>   
-        
-          
-        
-    </div>
-  </div>
-
-  <div>
-    <input v-model="point" type="text"> <span>POINT</span>
-  </div>
-  <div>
-    <button>충전하기</button>
-  </div>
-</div>
-
-    
-  
 </template>
+
+
 <script>
 export default {
-  name : 'PointView',
-  components: {},
-  data() {
-    return {
-      sampleData: ''
+  name: 'PointView',
+  data(){
+    return{
+      selected:0,   
+      userinfo : {
+        name: '',
+        email: '',
+      }
     }
   },
-  setup() {},
-  created() {},
-  mounted() {},
-  unmounted() {},
-  methods: {}
+  methods: {
+    requestPay : function() {
+      /* 1. 가맹점 식별하기 */
+      const { IMP } = window;
+      IMP.init('TC0ONETIME');
+
+      /* 2. 결제 데이터 정의하기 */
+       IMP.request_pay({
+        pg:'kakaopay',                       
+        pay_method: 'card',                     
+        merchant_uid: `mid_${new Date().getTime()}`, 
+        amount: this.selected,                                
+        name: '아임포트 결제',               
+        buyer_name: this.userinfo.name,                      
+        buyer_email: this.userinfo.email,               
+                   
+        
+      }, 
+        function(rsp) { // callback
+            let amount=this.amount
+            console.log(amount)
+            if (rsp.success) {
+            fetch('http://localhost:8080/users/points', {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization' : 'ad eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImlkIiwiaXNzIjoiZGVhcm1lIiwiaWF0IjoxNjU5NjgwMzg0LCJleHAiOjE2NTk2ODM5ODR9.izg6xcOPKIdxiU6QjTQ2GoVKFqSsBwBgyP4FCd6uibQ'
+                },
+                body: JSON.stringify({price: amount})
+            }).then((rsp) => rsp.json()).then((amount) => {
+                console.log(amount);
+            });
+            alert("완료 -> imp_uid : " + rsp.imp_uid+" / merchant_uid(orderKey) : " + rsp.merchant_uid);
+        
+      } else {
+        alert("실패 : 코드(" + rsp.error_code + ") / 메시지(" + rsp.error_msg + ")");
+        alert(`결제 실패`);
+      }
+    })
+    },
+  }
 }
+    //   rsp => {
+    //     const selected = this.selected
+    //     if(rsp.success){
+            // fetch('http://localhost:8080/users/points', {
+            //     method: 'PUT',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //         'Authorization' : 'ad eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImlkIiwiaXNzIjoiZGVhcm1lIiwiaWF0IjoxNjU5NjgwMzg0LCJleHAiOjE2NTk2ODM5ODR9.izg6xcOPKIdxiU6QjTQ2GoVKFqSsBwBgyP4FCd6uibQ'
+            //     },
+            //     body: JSON.stringify({price: selected})
+            // }).then((response) => response.json()).then((selected) => {
+            //     console.log(selected);
+            // });
+            // alert("완료 -> imp_uid : " + rsp.imp_uid+" / merchant_uid(orderKey) : " + rsp.merchant_uid);
+    //     }else{
+            // alert("실패 : 코드(" + rsp.error_code + ") / 메시지(" + rsp.error_msg + ")");
+    //     }
+    // }
+//   },
+// },
+
 </script>
-<style scoped>
-.point {
-  width : auto
-}
-</style>
