4966fb0131a2e1a7baae562c29015c7560f46559
이기종 <rl00822@naver.com>
2022-08-09 03:00:33 +0900

    [feat] : 알림 기능 대비 스케줄러 적용

--- back/demo/src/main/java/com/dearme/demo/DemoApplication.java
+++ back/demo/src/main/java/com/dearme/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @SpringBootApplication
 @EnableWebMvc
 @EnableJpaAuditing
+@EnableScheduling
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
