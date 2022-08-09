22e8b818ff6da9d30a4aba44695115c69632182a
Sangmeeeee <goodman1223@naver.com>
2022-08-09 18:10:59 +0900

    [feat] : cors allow

--- back/demo/src/main/java/com/dearme/demo/global/config/webserver/WebConfig.java
+++ back/demo/src/main/java/com/dearme/demo/global/config/webserver/WebConfig.java
@@ -26,6 +26,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
+                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://localhost:8081")
                 .allowedMethods("GET","POST","PUT","DELETE");
     }
 
