a2bb33eba2e7e4eefd37ba0f9927614facd81b9a
이기종 <rl00822@naver.com>
2022-08-09 21:14:55 +0900

    S07P12D206-490 [feat] : 영상 일기 저장 시 알림 스케줄러 생성 기능 구현

--- back/demo/src/main/java/com/dearme/demo/DemoApplication.java
+++ back/demo/src/main/java/com/dearme/demo/DemoApplication.java
@@ -1,25 +1,12 @@
 package com.dearme.demo;
 
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.json.JSONObject;
-import org.quartz.JobDataMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 
 
 @SpringBootApplication
@@ -28,9 +15,6 @@ import java.util.GregorianCalendar;
 @EnableScheduling
 public class DemoApplication {
     public static void main(String[] args) {
-
-
-
         SpringApplication.run(DemoApplication.class, args);
     }
 }
