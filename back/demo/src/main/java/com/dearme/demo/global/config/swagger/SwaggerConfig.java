80c67c7c00748008283e3e9dcf1dd9406b8433ba
Sangmeeeee <goodman1223@naver.com>
2022-08-09 18:38:45 +0900

    [feat] : swagger에 authorization 추가

--- back/demo/src/main/java/com/dearme/demo/global/config/swagger/SwaggerConfig.java
+++ back/demo/src/main/java/com/dearme/demo/global/config/swagger/SwaggerConfig.java
@@ -1,19 +1,47 @@
 package com.dearme.demo.global.config.swagger;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class SwaggerConfig {
     @Bean
     Docket api(){
         return new Docket(DocumentationType.OAS_30)
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .build();
+    }
+
+    private SecurityContext securityContext(){
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth(){
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
+    }
+
+    private ApiKey apiKey(){
+        return new ApiKey("Authorization", "Authorization", "header");
     }
 }
