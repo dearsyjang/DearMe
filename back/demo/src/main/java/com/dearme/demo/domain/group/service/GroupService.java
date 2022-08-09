3258f41225a12e8c9be223ca5aac857b6353d0a8
이상민 <goodman1223@naver.com>
2022-08-09 22:51:31 +0900

    S07P12D206-491/feature/groups/info 그룹 상세 정보 요청 API 개발

--- back/demo/src/main/java/com/dearme/demo/domain/group/service/GroupService.java
+++ back/demo/src/main/java/com/dearme/demo/domain/group/service/GroupService.java
@@ -2,7 +2,10 @@ package com.dearme.demo.domain.group.service;
 
 import com.dearme.demo.domain.group.dto.CreateGroupRequestDto;
 import com.dearme.demo.domain.group.dto.CreateGroupResponseDto;
+import com.dearme.demo.domain.group.dto.GroupInfoResponseDto;
 
 public interface GroupService {
     CreateGroupResponseDto createGroup(String id, CreateGroupRequestDto dto);
+
+    GroupInfoResponseDto getGroupInfo(Long groupId);
 }
