306388d5dec66796b4cd3400fc7a0af6c67e4997
Sangmeeeee <goodman1223@naver.com>
2022-08-11 00:04:44 +0900

    [feat] : OpenVidu Bean 등록

--- back/demo/src/main/java/com/dearme/demo/global/config/webserver/WebConfig.java
+++ back/demo/src/main/java/com/dearme/demo/global/config/webserver/WebConfig.java
@@ -3,7 +3,10 @@ package com.dearme.demo.global.config.webserver;
 import com.dearme.demo.global.converter.CounselorProfileRequestDtoConverter;
 import com.dearme.demo.global.util.jwt.AccessTokenInterceptor;
 import com.dearme.demo.global.util.jwt.RefreshTokenInterceptor;
+import io.openvidu.java.client.OpenVidu;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -21,6 +24,12 @@ public class WebConfig implements WebMvcConfigurer {
     private final AccessTokenInterceptor accessTokenInterceptor;
 
     private final RefreshTokenInterceptor refreshTokenInterceptor;
+
+    @Value("${openvidu.openvidu_url}")
+    private final String OPENVIDU_URL;
+
+    @Value("${openvidu.password}")
+    private final String PASSWORD;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -40,5 +49,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(refreshTokenInterceptor).addPathPatterns("/users/access-token");
         registry.addInterceptor(accessTokenInterceptor).excludePathPatterns(Arrays.asList(new String[]{"/users/token", "/users/id/{id}", "/users/nickname/{nickname}", "/users/id", "/users/password", "/h2-console", "/swagger-ui/*", "/swagger-resources", "/swagger-resources/**", "/v3/*", "/v3"}));
+    }
+
+    @Bean
+    public OpenVidu openVidu(){
+        return new OpenVidu(OPENVIDU_URL, PASSWORD);
     }
 }
