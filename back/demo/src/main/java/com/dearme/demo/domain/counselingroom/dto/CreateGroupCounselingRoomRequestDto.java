c2817e35091771e755d08df51c0e71cac6a7562a
goodman1223 <goodman1223@naver.com>
2022-08-11 16:08:33 +0900

    S07P12D206-503 [feat] : 그룹 화상방 생성 요청 로직 구현

---
+++ back/demo/src/main/java/com/dearme/demo/domain/counselingroom/dto/CreateGroupCounselingRoomRequestDto.java
@@ -0,0 +1,8 @@
+package com.dearme.demo.domain.counselingroom.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateGroupCounselingRoomRequestDto {
+    private Long groupId;
+}
--- back/demo/src/main/java/com/dearme/demo/domain/counselingroom/service/CounselingRoomService.java
+++ back/demo/src/main/java/com/dearme/demo/domain/counselingroom/service/CounselingRoomService.java
@@ -2,6 +2,7 @@ package com.dearme.demo.domain.counselingroom.service;
 
 import com.dearme.demo.domain.counselingroom.dto.CreateCounselingRoomRequestDto;
 import com.dearme.demo.domain.counselingroom.dto.CreateCounselingRoomResponseDto;
+import com.dearme.demo.domain.counselingroom.dto.CreateGroupCounselingRoomRequestDto;
 import com.dearme.demo.domain.counselingroom.dto.GetSessionTokenResponseDto;
 import io.openvidu.java.client.OpenViduHttpException;
 import io.openvidu.java.client.OpenViduJavaClientException;
@@ -10,4 +11,6 @@ public interface CounselingRoomService {
     CreateCounselingRoomResponseDto createCounselingRoom(String id, CreateCounselingRoomRequestDto dto) throws OpenViduJavaClientException, OpenViduHttpException;
 
     GetSessionTokenResponseDto getSessionToken(String id, Long counselingId);
+
+    CreateCounselingRoomResponseDto createGroupCounselingRoom(String id, CreateGroupCounselingRoomRequestDto dto) throws OpenViduJavaClientException, OpenViduHttpException;
 }
