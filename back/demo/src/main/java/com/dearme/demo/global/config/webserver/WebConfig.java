73925425ddef4d24373930ba0031eac8aadaca62
Sangmeeeee <goodman1223@naver.com>
2022-08-12 01:21:05 +0900

    [fix] : 사용자 이미지 interceptor에서 제외

--- back/demo/src/main/java/com/dearme/demo/global/config/webserver/WebConfig.java
+++ back/demo/src/main/java/com/dearme/demo/global/config/webserver/WebConfig.java
@@ -47,8 +47,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(refreshTokenInterceptor).addPathPatterns("/users/access-token");
-       registry.addInterceptor(accessTokenInterceptor).excludePathPatterns(Arrays.asList(new String[]{"/users/token", "/users/id/{id}", "/users/nickname/{nickname}", "/users/id", "/users/password", "/h2-console", "/swagger-ui/*", "/swagger-resources", "/swagger-resources/**", "/v3/*", "/v3"}));
+        registry.addInterceptor(refreshTokenInterceptor).addPathPatterns("/users/access-token").excludePathPatterns("/users/image");
+       registry.addInterceptor(accessTokenInterceptor).excludePathPatterns(Arrays.asList(new String[]{"/users/token", "/users/id/{id}", "/users/nickname/{nickname}", "/users/id", "/users/password", "/h2-console", "/swagger-ui/*", "/swagger-resources", "/swagger-resources/**", "/v3/*", "/v3", "/users/image"}));
     }
 
     @Bean
