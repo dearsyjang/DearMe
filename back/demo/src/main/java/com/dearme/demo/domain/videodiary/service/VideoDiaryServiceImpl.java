259158e3bb49f7719bbc23c414b26f8a9522cebe
이기종 <rl00822@naver.com>
2022-08-12 10:27:49 +0900

    [fix] : STT 요청 부분 리눅스 명령어 수정 v28

--- back/demo/src/main/java/com/dearme/demo/domain/videodiary/service/VideoDiaryServiceImpl.java
+++ back/demo/src/main/java/com/dearme/demo/domain/videodiary/service/VideoDiaryServiceImpl.java
@@ -143,14 +143,76 @@ public class VideoDiaryServiceImpl implements VideoDiaryService {
     }
 
 
-    public String[] videoSTT(String path) throws IOException {
+    public String[] videoSTT(String path) throws IOException, InterruptedException {
         //서버에서 실행시킬 때
 
         String filePath = "/home/ubuntu/docker-volume/video/" + path + "/" + path;
 
-        String [] cmd = new String[] {"sh", "-c", "sudo mkdir asdf"};
-        Runtime rt = Runtime.getRuntime();
-        Process proc = rt.exec(cmd);
+        List cmdList = new ArrayList();
+        cmdList.add("dir");
+        Process process = null;
+        String str = null;
+
+        try {
+            process = new ProcessBuilder(cmdList).directory(new File("home/ubuntu")).start();
+            BufferedReader stdOut = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            while((str = stdOut.readLine()) != null) {
+                System.out.println(str);
+            }
+            int exitCode = process.waitFor();
+            System.out.println("\nExited with error code : " + exitCode);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        List cmdList2 = new ArrayList();
+        cmdList2.add("dir");
+        Process process2 = null;
+        String str2 = null;
+
+        try {
+            process2 = new ProcessBuilder(cmdList2).directory(new File("/home/ubuntu")).start();
+            BufferedReader stdOut = new BufferedReader(new InputStreamReader(process2.getInputStream()));
+
+            while((str2 = stdOut.readLine()) != null) {
+                System.out.println(str2);
+            }
+            int exitCode = process2.waitFor();
+            System.out.println("\nExited with error code : " + exitCode);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        List<String> cmdList4 = new ArrayList<String>();
+        cmdList4.add("dir");
+        cmdList4.add("home");
+        Process process4 = null;
+        String str4 = null;
+
+        try {
+            process4 = new ProcessBuilder(cmdList4).start();
+            BufferedReader stdOut = new BufferedReader(new InputStreamReader(process4.getInputStream()));
+
+            while((str4 = stdOut.readLine()) != null) {
+                System.out.println(str4);
+            }
+            int exitCode = process4.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        String[] cmd = new String[] { "dir"
+                ,"home"
+        };
+
+        ProcessBuilder bld = new ProcessBuilder(cmd);
+
+        Process process3 = bld.start();
+        int exitCode =  process3.waitFor();
+        System.out.println(exitCode);
+
+
 
 
 
