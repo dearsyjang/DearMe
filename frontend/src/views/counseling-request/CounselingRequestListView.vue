3e2b0dd26ce8a15ed0e9516f4b882b5e24f02597
강지명 <rainbow7417@naver.com>
2022-08-13 21:19:43 +0900

    S07P12D206-300 [feat] : 상담 신청서 모달

--- frontend/src/views/counseling-request/CounselingRequestListView.vue
+++ frontend/src/views/counseling-request/CounselingRequestListView.vue
@@ -1,29 +1,39 @@
 <template>
   <div>
+    <br><br><br><br>
+    <h1>수락 대기 중인 상담</h1>
+    <hr>
+    <br><br>
     <div>
         <div>
-            <h1>1:1 상담</h1>
+            <h2>1:1 상담</h2>
               <div>
-                <personalcard-comp></personalcard-comp>
+                <personalcard-comp 
+                v-for="(request,idx) in requests.data"
+                :key="idx"
+                :request="request">
+                </personalcard-comp>
               </div>
         </div>
     </div>
-
+    <br>
     <div>
         <div>
-          <h1>그룹 상담</h1>
+          <h2>그룹 상담</h2>
         </div>
           <div>
             <groupcard-comp></groupcard-comp>
           </div>
           
     </div>
-    
+    <br><br><br><br><br><br><br>
   </div>
 </template>
 <script>
 import personalcardComp from '@/views/counseling-request/components/personalcardComp.vue'
 import groupcardComp from '@/views/counseling-request/components/groupcardComp.vue'
+
+import { mapGetters,mapActions } from 'vuex'
 export default {
   name : 'CounselingRequestList',
   components: {
@@ -36,9 +46,18 @@ export default {
     }
   },
   setup() {},
-  created() {},
+
   mounted() {},
   unmounted() {},
-  methods: {}
+  computed:{
+    ...mapGetters(['requests','currentUser']),
+  },
+  methods: {
+    ...mapActions(['fetchRequests', 'fetchCurrentUser']),
+  },
+  created() {
+    this.fetchCurrentUser()
+    this.fetchRequests()
+}
 }
 </script>
