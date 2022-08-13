1d16157dcc3f3e40720ce5aa622fcb2922a4a2a4
이기종 <rl00822@naver.com>
2022-08-13 16:53:12 +0900

    [fix] : CounselorSearchRequestDto @NotBlank 수정

--- back/demo/src/main/java/com/dearme/demo/domain/user/dto/counselorsearch/CounselorSearchRequestDto.java
+++ back/demo/src/main/java/com/dearme/demo/domain/user/dto/counselorsearch/CounselorSearchRequestDto.java
@@ -11,15 +11,10 @@ public class CounselorSearchRequestDto {
     private Long upPrice;
     @NotBlank(message = "downPrice는 비워둘 수 없습니다.")
     private Long downPrice;
-    @NotBlank(message = "reviewCntUp은 비워둘 수 없습니다.")
     private Boolean reviewCntUp;
-    @NotBlank(message = "reviewCntDown은 비워둘 수 없습니다.")
     private Boolean reviewCntDown;
-    @NotBlank(message = "reviewScoreUp은 비워둘 수 없습니다.")
     private Boolean reviewScoreUp;
-    @NotBlank(message = "reviewScoreDown은 비워둘 수 없습니다.")
     private Boolean reviewScoreDown;
-    @NotBlank(message = "favorite은 비워둘 수 없습니다.")
     private Boolean favorite;
 
 }
