ae871812b00da6c7c5c7c28046be067e09f9bc23
강지명 <rainbow7417@naver.com>
2022-08-14 06:03:26 +0900

    Revert "Merge branch 'S07P12D206-300/feature/couseling/counselingDocument' into 'master'"
    This reverts merge request !179

--- frontend/src/router/videoDiary.js
+++
@@ -1,8 +0,0 @@
-import VideoDiaryCreateView from '@/views/video-diary/VideoDiaryCreateView.vue'
-
-export default [
-  {
-    path: '/videodiary',
-    name: 'videodiary',
-    component: VideoDiaryCreateView
-  }]
