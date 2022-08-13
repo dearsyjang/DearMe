db426cbba4333dd59edd8d9e2523d42d865993ed
rainbow7417 <rainbow7417@naver.com>
2022-08-14 05:54:39 +0900

    S07P12D206-300 [feat] : 상담 신청서 모달 상담사 상담 일정(일정달력, 오늘의 상담)

--- frontend/src/router/counselingSchedule.js
+++ frontend/src/router/counselingSchedule.js
@@ -1,16 +1,17 @@
-import userSchedule from '@/views/counselingSchedule/UserScheduleView.vue'
-import counselorSchedule from '@/views/counselingSchedule/CounselorScheduleView.vue'
 
+import counselorScheduleView from '@/views/counselingSchedule/CounselorScheduleView.vue'
+import TodayScheduleView from '@/views/counselingSchedule/TodayScheduleView.vue'
 export default [
-  {
-    path: '/counseling-schedule/user-schedule',
-    name: 'userSchedule',
-    component: userSchedule
-    },
+
   {
     path: '/counseling-schedule/counselor-schedule',
     name: 'counselorSchedule',
-    component: counselorSchedule
+    component: counselorScheduleView
+  },
+  {
+    path: '/counseling-schedule/:today',
+    name: 'todaySchedule',
+    component: TodayScheduleView
   },
      
 ]
