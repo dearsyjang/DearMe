cb0741d7c0cbfda3a6a0a658e9b5262334eb941c
정윤해 <ue0828@naver.com>
2022-08-15 20:54:04 +0900

    Resolve S07P12D206-543 "/feature/design/group"

--- frontend/src/views/group/GroupDetailView.vue
+++ frontend/src/views/group/GroupDetailView.vue
@@ -1,19 +1,64 @@
 <template>
-  <div>
-    <group-info :group="group" class="groupInfo"></group-info>
-    <group-content :group="group" class="groupContent"></group-content>
-    <p>{{ group }}</p>
-    <p></p>
+  <div class="page-content-wrapper py-3 group-bg">
+    <div class="container">
+      <div class="card my-2">
+        <div class="card-body p-2">
+          <div class="d-flex align-items-center justify-content-between">
+            <small class="ms-1 board-text-index">그룹 상세</small>
+            <div>
+              <router-link to="/board"><button class="board-btn-index btn-sm mx-2">목록</button></router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card product-details-card mb-3 direction-rtl">
+        <div class="card-body">
+
+          <h1>{{ group.counselor }} 상담사</h1>
+          <p>최고의 상담사</p>
+          <form action="#" class="text-center">
+            <router-link :to="{name: 'counselorProfile', params: {counselorId: this.group.counselorId}}">
+              <button class="btn btn-primary w-50" type="submit">상담사 프로필</button>
+            </router-link>
+
+          </form>
+        </div>
+      </div>
+
+      <div class="card product-details-card mb-3 direction-rtl">
+        <div class="card-body">
+          <h5>그룹 공지사항</h5>
+          <p>{{ group.contents }}</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum soluta tempore tenetur provident eligendi
+            porro, eius nulla? Aliquam, blanditiis id. Corporis.</p>
+          <p class="mb-0">Lorem ipsum dolor sit, amet consectetur adipisicing elit. At ut fugit accusantium quo quidem
+            magni laboriosam!</p>
+
+
+
+      <div class="card related-product-card direction-rtl my-3 p-0 price-card">
+        <div class="card-body">
+          <div class="d-flex justify-content-center">
+            <p class="row badge bg-primary mx-3">price</p>
+            <p class="text-center group-price">₩ {{ group.price }}</p>
+          </div>
+          <form action="#" class="text-center">
+            <router-link :to="{name: 'groupRequest', params: {groupId: this.group.id}}">
+              <button class="btn btn-primary">그룹 참가신청</button>
+            </router-link>
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
+  </div>
+</div>
 </template>
 <script>
 import { mapGetters, mapActions } from 'vuex'
-import groupContent from "./components/groupContent.vue";
-import groupInfo from "./components/groupInfo.vue";
+
 export default {
   components: {
-    groupContent,
-    groupInfo,
   },
   data() {
     return {
@@ -36,12 +81,19 @@ export default {
   },
 };
 </script>
-<style scoped>
-.groupInfo {
-  height: 20%;
-  margin-bottom: 20px;
+<style>
+.group-btn {
+  background-color: #504E48;
+  border: none;
+
 }
-.groupContent {
-  height: 70%;
+.group-bg{
+  background-color: #F9F7F7;
+}
+.group-price{
+  font-size: large;
+}
+.price-card {
+  background-color: #F9F7F7;
 }
 </style>
