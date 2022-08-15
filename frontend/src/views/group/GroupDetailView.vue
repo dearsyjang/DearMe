eeca7852b74898b60238eae0aca87714b0cb6dc4
YOONHAE <ue0828@naver.com>
2022-08-15 23:32:17 +0900

    S07P12D206 [feat] : 그룹 참가 신청 에러

--- frontend/src/views/group/GroupDetailView.vue
+++ frontend/src/views/group/GroupDetailView.vue
@@ -4,53 +4,47 @@
       <div class="card my-2">
         <div class="card-body p-2">
           <div class="d-flex align-items-center justify-content-between">
-            <small class="ms-1 board-text-index">그룹 상세</small>
+            <small class="ms-1 board-text-index">그룹 상담 상세</small>
             <div>
-              <router-link to="/board"><button class="board-btn-index btn-sm mx-2">목록</button></router-link>
+              <router-link to="/counselors/counselorList"><button class="board-btn-index btn-sm mx-2">목록</button></router-link>
             </div>
           </div>
         </div>
       </div>
       <div class="card product-details-card mb-3 direction-rtl">
         <div class="card-body">
-
-          <h1>{{ group.counselor }} 상담사</h1>
-          <p>최고의 상담사</p>
+          <h1 class="text-center">{{ group.counselor }} 상담사</h1>
+          <p class="text-center">최고의 상담사</p>
           <form action="#" class="text-center">
             <router-link :to="{name: 'counselorProfile', params: {counselorId: this.group.counselorId}}">
-              <button class="btn btn-primary w-50" type="submit">상담사 프로필</button>
+              <button class="w-btn-neon2 " type="button">상담사 프로필</button>
             </router-link>
-
           </form>
         </div>
       </div>
-
       <div class="card product-details-card mb-3 direction-rtl">
         <div class="card-body">
           <h5>그룹 공지사항</h5>
           <p>{{ group.contents }}</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum soluta tempore tenetur provident eligendi
+          <p>공지사항이 짧아서 아무글 써놨음 Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum soluta tempore tenetur provident eligendi
             porro, eius nulla? Aliquam, blanditiis id. Corporis.</p>
           <p class="mb-0">Lorem ipsum dolor sit, amet consectetur adipisicing elit. At ut fugit accusantium quo quidem
             magni laboriosam!</p>
-
-
-
-      <div class="card related-product-card direction-rtl my-3 p-0 price-card">
-        <div class="card-body">
-          <div class="d-flex justify-content-center">
-            <p class="row badge bg-primary mx-3">price</p>
-            <p class="text-center group-price">₩ {{ group.price }}</p>
+        <div class="card related-product-card direction-rtl my-3 p-0 ">
+          <div class="card-body">
+            <div class="d-flex justify-content-center">
+              <p class="row badge bg-primary mx-3 group-price-btn">price</p>
+              <p class="text-center group-price">₩ {{ group.price }}</p>
+            </div>
+            <form action="#" class="text-center">
+              <router-link :to="{name: 'groupRequest', params: {groupId: this.group.id}}">
+                <button class="w-btn-neon2 " type="button">그룹 참가신청</button>
+              </router-link>
+            </form>
           </div>
-          <form action="#" class="text-center">
-            <router-link :to="{name: 'groupRequest', params: {groupId: this.group.id}}">
-              <button class="btn btn-primary">그룹 참가신청</button>
-            </router-link>
-          </form>
         </div>
       </div>
     </div>
-  </div>
   </div>
 </div>
 </template>
@@ -93,7 +87,43 @@ export default {
 .group-price{
   font-size: large;
 }
+.group-price-btn {
+  border: none;
+    background: linear-gradient(
+        90deg,
+        #4a5069 0%,
+        #293462 100%
+    );
+    border-radius: 1000px;
+    color: darkslategray;
+    cursor: pointer;
+    /* box-shadow: 12px 12px 24px #293462; */
+    font-weight: 700;
+    transition: 0.3s;
+}
 .price-card {
   background-color: #F9F7F7;
 }
+.w-btn-neon2 {
+    /* position: relative; */
+    border: none;
+    min-width: 200px;
+    min-height: 50px;
+    background: linear-gradient(
+        90deg,
+        #B2C8DF 0%,
+        #86b2e1 100%
+    );
+    border-radius: 1000px;
+    color: darkslategray;
+    cursor: pointer;
+    box-shadow: 12px 12px 24px #B2C8DF;
+    font-weight: 700;
+    transition: 0.3s;
+}
+
+.w-btn-neon2:hover {
+    transform: scale(1.2);
+}
+
 </style>
